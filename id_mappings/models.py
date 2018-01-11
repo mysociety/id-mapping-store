34aa610bc436503f2e904a21731c5de7a180fd1f
Mark Longair <mhl@pobox.com>
2018-01-11 17:52:56 +0000

    Add models and initial migration for id_mappings

--- id_mappings/models.py
+++ id_mappings/models.py
@@ -3,4 +3,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Scheme(models.Model):
+    name = models.CharField(max_length=512)
+
+
+class Identifier(models.Model):
+    value = models.CharField(max_length=512)
+    scheme = models.ForeignKey(Scheme)
+
+
+class EquivalenceClaim(models.Model):
+    identifier_a = models.ForeignKey(Identifier, related_name='claims_via_a')
+    identifier_b = models.ForeignKey(Identifier, related_name='claims_via_b')
+    created = models.DateTimeField()
+    deprecated = models.BooleanField(default=False)
