19e966ca0f98147244f95fc064f3b06b89480457
Mark Longair <mhl@pobox.com>
2018-01-12 15:31:57 +0000

    Factor out fixture creation into a mixin

--- id_mappings/tests.py
+++ id_mappings/tests.py
@@ -7,20 +7,25 @@ from django.test import Client, TestCase
 
 from id_mappings.models import EquivalenceClaim, Identifier, Scheme
 
-class TestIdentiferLookup(TestCase):
+
+class FixtureMixin(object):
 
     def setUp(self):
+        super(FixtureMixin, self).setUp()
         self.area_scheme = Scheme.objects.create(name='uk-area_id')
         self.wd_district_scheme = Scheme.objects.create(name='wikidata-district-item')
-        area_identifier = Identifier.objects.create(
+        self.area_identifier = Identifier.objects.create(
             value='gss:S17000017',
             scheme=self.area_scheme)
-        wd_identifier = Identifier.objects.create(
+        self.wd_identifier = Identifier.objects.create(
             value='Q1529479',
             scheme=self.wd_district_scheme)
         EquivalenceClaim.objects.create(
-            identifier_a=area_identifier,
-            identifier_b=wd_identifier)
+            identifier_a=self.area_identifier,
+            identifier_b=self.wd_identifier)
+
+
+class TestIdentiferLookup(FixtureMixin, TestCase):
 
     def test_identifier_not_found_via_scheme_name(self):
         c = Client()
