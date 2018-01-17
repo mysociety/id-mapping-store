ac13abd0a16e24d08656eee40a5b5272a4be0951
Louise Crow <louise.crow@gmail.com>
2018-01-17 15:34:14 +0000

    Set up some useful paths

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -12,12 +12,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import yaml
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from .paths import *
 
 def get_conf(conf_file_leafname):
-    full_filename = os.path.join(BASE_DIR, 'conf', conf_file_leafname)
+    full_filename = os.path.join(PROJECT_ROOT, 'conf', conf_file_leafname)
     with open(full_filename) as f:
         return yaml.load(f)
 
